b6db1b3a9569b310e6028af47323c18eedbe22e2
Bryce Allen <bdallen@uchicago.edu>
2020-05-13 16:00:28 -0400

    expr: show working expr and not working expr

--- expr.cxx
+++ expr.cxx
@@ -26,6 +26,8 @@
 int main(int argc, char **argv) {
     constexpr std::size_t N = 16;
     std::vector<double> h_x(N);
+    std::vector<double> h_y(N);
+    std::vector<double> h_z(N);
 
     for (int i=0; i<N; i++) {
         h_x[i] = static_cast<double>(i);
@@ -50,7 +52,7 @@ int main(int argc, char **argv) {
 
     auto k_fun = binaryclosure<plus<double>, double>(plus<double>{}, 7, 11);
 
-    using dconst = constfn<double, size_t>;
+    using dconst = constfn<double, double>;
     using dlinear = linearfn<double, double>;
     auto k_expr_const = mkexpr(plus<double>{}, dconst{2.0}, dconst{7.5});
     auto k_expr_linear = mkexpr(plus<double>{}, dlinear{2.0, 1.0},
@@ -61,18 +63,25 @@ int main(int argc, char **argv) {
     auto k_expr_mult = mkexpr(times<double>{}, dlinear{2.0, 0.0},
                               dlinear{2.0, 0.0});
     {
-        sycl::buffer<double, 1> buf{h_x.data(), sycl::range<1>{N}};
+        sycl::buffer<double, 1> buf_x{h_x.data(), sycl::range<1>{N}};
+        sycl::buffer<double, 1> buf_y{h_y.data(), sycl::range<1>{N}};
+        sycl::buffer<double, 1> buf_z{h_z.data(), sycl::range<1>{N}};
+
         auto e = q.submit([&](sycl::handler &cgh) {
-          auto acc = buf.get_access<sycl::access::mode::write>(cgh);
+          auto acc_x = buf_x.get_access<sycl::access::mode::write>(cgh);
+          auto acc_y = buf_y.get_access<sycl::access::mode::write>(cgh);
+          auto acc_z = buf_z.get_access<sycl::access::mode::write>(cgh);
           cgh.parallel_for(sycl::range<1>(N),
           [=](sycl::item<1> item) mutable {
              int i = item.get_linear_id();
-             acc[i] = k_expr_complex(i);
+             acc_x[i] = k_expr_const(i);
+             acc_y[i] = k_expr_linear(i);
+             acc_z[i] = k_expr_complex(i);
           });
         });
     }
 
     for (int i=0; i<N; i++) {
-        std::cout << h_x[i] << std::endl;
+        std::cout << h_x[i] << " + " << h_y[i] << " = " << h_z[i] << std::endl;
     }
 }
