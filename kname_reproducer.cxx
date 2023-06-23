50aba82f1d28a39e9efa3a01ff38d0c614906925
Bryce Allen <bdallen@uchicago.edu>
2023-06-23 16:40:45 -0400

    remove external SyclCPLX dep for kname reproducer

--- kname_reproducer.cxx
+++ kname_reproducer.cxx
@@ -3,90 +3,85 @@
 
 #include <sycl/sycl.hpp>
 
-#define _SYCL_CPLX_NAMESPACE gt::sycl_cplx
-#include "sycl_ext_complex.hpp"
-
 // replicate how gtensor handles complex types, with gt::complex<T> alias
-namespace gt
-{
+namespace sycl_cplx {
 
-template <typename T>
-using complex = gt::sycl_cplx::complex<T>;
+template <class _Tp, class _Enable = void> class complex;
 
-using gt::sycl_cplx::pow;
+template <class _Tp>
+class complex<
+    _Tp, typename std::enable_if<std::is_floating_point<_Tp>::value>::type> {
+public:
+  typedef _Tp value_type;
 
-template <typename T1, typename T2>
-struct KName;
+  constexpr complex(value_type __re = value_type(),
+                    value_type __im = value_type())
+      : __re_(__re), __im_(__im) {}
 
-} // namespace gt
+private:
+  value_type __re_;
+  value_type __im_;
+};
 
+} // namespace sycl_cplx
 
-template <typename DerivedExpression>
-class expression {
+template <typename DerivedExpression> class expression {
 protected:
   expression() = default;
 
 public:
   using derived_type = DerivedExpression;
 
-  const derived_type& derived() const&;
-  derived_type& derived() &;
+  const derived_type &derived() const &;
+  derived_type &derived() &;
   derived_type derived() &&;
 };
 
-
 template <typename DerivedExpression>
-inline auto expression<DerivedExpression>::derived() const&
--> const derived_type& {
-    return static_cast<const derived_type&>(*this);
+inline auto
+expression<DerivedExpression>::derived() const & -> const derived_type & {
+  return static_cast<const derived_type &>(*this);
 }
 
-
 template <typename DerivedExpression>
-inline auto expression<DerivedExpression>::derived() & -> derived_type& {
-    return static_cast<derived_type&>(*this);
+inline auto expression<DerivedExpression>::derived() & -> derived_type & {
+  return static_cast<derived_type &>(*this);
 }
-
 
 template <typename DerivedExpression>
 inline auto expression<DerivedExpression>::derived() && -> derived_type {
-    return static_cast<derived_type&&>(*this);
+  return static_cast<derived_type &&>(*this);
 }
 
+template <typename T> class container : public expression<container<T>> {
+public:
+  using value_type = std::decay_t<T>;
+  using pointer = std::add_pointer_t<value_type>;
+  using reference = std::add_lvalue_reference_t<value_type>;
+  using self_type = container<T>;
+  using base_type = expression<self_type>;
 
-template <typename T>
-class container : public expression<container<T>> {
-  public:
-    using value_type = std::decay_t<T>;
-    using pointer = std::add_pointer_t<value_type>;
-    using reference = std::add_lvalue_reference_t<value_type>;
-    using self_type = container<T>;
-    using base_type = expression<self_type>;
+  container(std::size_t count, pointer data) : count_(count), data_(data) {}
 
-    container(std::size_t count, pointer data) : count_(count), data_(data)
-    {}
+  reference operator[](std::size_t i) const { return data_[i]; };
+  reference operator()(std::size_t i) const { return data_[i]; };
 
-    reference operator[] (std::size_t i) const { return data_[i]; };
-    reference operator() (std::size_t i) const { return data_[i]; };
+  std::size_t size() const { return count_; }
 
-    std::size_t size() const { return count_; }
-
-  private:
-    pointer data_;
-    std::size_t count_;
+private:
+  pointer data_;
+  std::size_t count_;
 };
 
-template <typename E1, typename E2>
-class Assign1;
+template <typename E1, typename E2> class Assign1;
 
 // Note: passing expressions by value works
 // void assign(E1 lhs, const E2 rhs, sycl::queue &q)
 
 template <typename E1, typename E2>
-void assign(E1 &lhs, const E2& rhs, sycl::queue &q)
-{
+void assign(E1 &lhs, const E2 &rhs, sycl::queue &q) {
   std::size_t size = lhs.size();
-  auto e = q.submit([&](sycl::handler& cgh) {
+  auto e = q.submit([&](sycl::handler &cgh) {
     using ltype = decltype(lhs);
     using rtype = decltype(rhs);
     using kname = Assign1<ltype, rtype>;
@@ -97,50 +92,46 @@ void assign(E1 &lhs, const E2& rhs, sycl::queue &q)
   });
 }
 
-template <typename T>
-void test() {
-    constexpr std::size_t N = 16;
+template <typename T> void test() {
+  constexpr std::size_t N = 16;
 
-    auto q = sycl::queue{};
-    auto dev = q.get_device();
-    std::string type;
-    if (dev.is_cpu()) {
-        type = "CPU  ";
-    } else if (dev.is_gpu()) {
-        type = "GPU  ";
-    } else {
-        type = "OTHER";
-    }
-    std::cout << "[" << type << "] "
-              << dev.get_info<sycl::info::device::name>()
-              << " {" << dev.get_info<sycl::info::device::vendor>() << "}"
-              << std::endl;
+  auto q = sycl::queue{};
+  auto dev = q.get_device();
+  std::string type;
+  if (dev.is_cpu()) {
+    type = "CPU  ";
+  } else if (dev.is_gpu()) {
+    type = "GPU  ";
+  } else {
+    type = "OTHER";
+  }
+  std::cout << "[" << type << "] " << dev.get_info<sycl::info::device::name>()
+            << " {" << dev.get_info<sycl::info::device::vendor>() << "}"
+            << std::endl;
 
-    auto Adata = sycl::malloc_shared<T>(N, q);
-    auto A = container<T>(N, Adata);
-    auto Bdata = sycl::malloc_shared<T>(N, q);
-    auto B = container<T>(N, Bdata);
+  auto Adata = sycl::malloc_shared<T>(N, q);
+  auto A = container<T>(N, Adata);
+  auto Bdata = sycl::malloc_shared<T>(N, q);
+  auto B = container<T>(N, Bdata);
 
-    for (int i = 0; i < N; i++) {
-      B[i] = T{1.0 * i};
-    }
+  for (int i = 0; i < N; i++) {
+    B[i] = T{1.0 * i};
+  }
 
-    assign(A, B, q);
-    
-    /*
-    std::size_t size = A.size();
-    auto e = q.submit([&](sycl::handler& cgh) {
-      using ltype = decltype(A);
-      using rtype = decltype(B);
-      using kname = Assign1<ltype, rtype>;
-      cgh.parallel_for<kname>(sycl::range<1>(size), [=](sycl::item<1> item) {
-        auto i = item.get_id();
-        A(i) = B(i);
-      });
+  assign(A, B, q);
+
+  /*
+  std::size_t size = A.size();
+  auto e = q.submit([&](sycl::handler& cgh) {
+    using ltype = decltype(A);
+    using rtype = decltype(B);
+    using kname = Assign1<ltype, rtype>;
+    cgh.parallel_for<kname>(sycl::range<1>(size), [=](sycl::item<1> item) {
+      auto i = item.get_id();
+      A(i) = B(i);
     });
-    */
+  });
+  */
 }
 
-int main(int argc, char **argv) {
-  test<gt::complex<double>>();
-}
+int main(int argc, char **argv) { test<sycl_cplx::complex<double>>(); }
